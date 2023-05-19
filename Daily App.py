729bb4aee135f415871ebf8149aff90982e45618
Your Name <ltp5145@psu.edu>
2023-05-19 15:59:22 -0400

    first parts of daily entry

--- Daily App.py
+++ Daily App.py
@@ -1,4 +1,7 @@
 import kivy
+import datetime
+import random
+import time
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -6,6 +9,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
 
 Config.set('graphics', 'resizable', True)
 Config.set('graphics', 'width', '400')
@@ -14,8 +20,24 @@ Config.set('graphics', 'height', '700')
 class HomePage(Screen):
     pass
 
-class DailyEntryPage(Screen):
-    pass
+class DailyEntryPage(Screen,Widget):
+    # Callback for the checkbox
+    def checkbox_click(self, instance, value):
+        def hide_label(dt):
+            self.ids.motivation.text=""
+
+        words=["Way to go!","Good job!","Awesome work!","Well done!"]
+        ran_num=random.randrange(len(words))
+
+        #Value is true or false depending on if checkbox is checked or not
+        if value:
+            self.ids.motivation.text=words[ran_num]
+            Clock.schedule_once(hide_label, 2)
+            
+        else:
+            self.ids.motivation.text=""
+        
+    
 
 
 class PastEntriesPage(Screen):
@@ -25,8 +47,7 @@ class WindowManager(ScreenManager):
     pass
 
 
-class Popup1(FloatLayout):
-    pass
+
 
 
 
